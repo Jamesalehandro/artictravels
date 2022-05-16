81861b075078f12fafa2a220c0877fdfdf5baaf3
Jamesalehandro <jamesalehandro@gmail.com>
2022-05-17 00:05:53 +0100

    Updated

--- src/components/Section4/Section4.jsx
+++ src/components/Section4/Section4.jsx
@@ -1,79 +1,88 @@
-import React from "react";
-import { Box, Button, Flex, Image, Text, VStack } from "@chakra-ui/react";
-import palmTree from "../../assets/Images/picture.png";
-import boat from "../../assets/Images/boat.png";
-import snow from "../../assets/Images/snow.png";
-import property from "../../assets/Images/Property.png";
+import React from 'react';
+import { Box, Button, Flex, Image, Text, VStack } from '@chakra-ui/react';
+import palmTree from '../../assets/Images/picture.png';
+import boat from '../../assets/Images/boat.png';
+import snow from '../../assets/Images/snow.png';
+import property from '../../assets/Images/Property.png';
 
 const Section4 = () => {
   return (
-    <Box w={"100%"} minH={"700px"} display={"flex"} pt={20} mb={20}>
+    <Box
+      w={'100%'}
+      minH={'700px'}
+      display={'flex'}
+      pt={20}
+      mb={20}
+      flexDirection={['column', 'column', 'column', 'row']}
+    >
       <VStack
-        w={"50%"}
-        minH={"700px"}
+        w={['100%', '100%', '100%', '50%', '50%']}
+        minH={'700px'}
         align="flex-start"
-        pl={20}
+        pl={[0, 0, 0, 20]}
+        border={'1px solid black'}
         sx={{
-          "@media (min-width:1920px) ": {
-            alignItems: "flex-end",
+          '@media (min-width:1920px) ': {
+            alignItems: 'flex-end',
             pl: 0,
           },
         }}
       >
         <Text
-          fontWeight={"bold"}
-          fontSize={"30px"}
+          fontWeight={'bold'}
+          fontSize={'30px'}
           lineHeight="54px"
-          w={"570px"}
-          textAlign={"left"}
-          pl={20}
+          w={['351px', '410px', '551px', '451px', '491px']}
+          textAlign={'left'}
+          pl={[10, 10, 10, 0, 20]}
+          border={'1px solid black'}
         >
           The best resorts to chill and relax
         </Text>
 
         <Flex
           width="100%"
-          minH={"200px"}
+          minH={'200px'}
           direction="column"
-          align={"flex-end"}
+          align={['center', 'center', 'center', 'flex-end']}
           pr={4}
         >
-          <Image src={palmTree} height={"500px"} mb={6} objectFit="cover" />
-          <Image src={boat} height={"500px"} mb={6} objectFit="cover" />
+          <Image src={palmTree} height={'500px'} mb={6} objectFit="cover" />
+          <Image src={boat} height={'500px'} mb={6} objectFit="cover" />
         </Flex>
       </VStack>
 
-      <VStack w={"50%"} minH={"700px"}>
+      <VStack w={['100%', '100%', '100%', '50%', '50%']} minH={'700px'}>
         <Flex
           width="100%"
-          minH={"200px"}
+          minH={'200px'}
           direction="column"
-          align={"flex-start"}
-          pl={4}
+          align={['center', 'center', 'center', 'flex-start']}
+          pl={[0, 0, 0, 4]}
           pt={2}
         >
           <Box
-            position={"relative"}
+            position={'relative'}
             mb={6}
-            borderRadius={"10px"}
+            borderRadius={'10px'}
             _after={{
               content: "''",
-              position: "absolute",
+              position: 'absolute',
               top: 0,
               left: 0,
-              width: "100%",
-              height: "100%",
-              borderRadius: "15px",
+              width: '100%',
+              height: '100%',
+              borderRadius: '15px',
               backgroundImage:
-                "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.563442) 60.97%, rgba(0, 0, 0, 0.65) 100%)",
+                'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.563442) 60.97%, rgba(0, 0, 0, 0.65) 100%)',
             }}
           >
-            <Image src={property} height={"500px"} objectFit="cover" />
+            <Image src={property} height={'500px'} objectFit="cover" />
 
             <VStack
               position="absolute"
               bottom="30px"
-              w={"100%"}
+              w={'100%'}
               //   border="2px solid red"
               pl={5}
               align="flex-start"
@@ -81,24 +90,24 @@ const Section4 = () => {
               gap={2}
             >
               <Text
-                w={"255px"}
-                height={"25px"}
+                w={'255px'}
+                height={'25px'}
                 fontSize="18px"
                 lineHeight="40px"
                 fontWeight={600}
-                color={"white"}
+                color={'white'}
                 zIndex={200000}
               >
                 Winter landscape Chalet
               </Text>
 
               <Text
-                w={"255px"}
-                height={"25px"}
+                w={'255px'}
+                height={'25px'}
                 fontSize="14px"
                 lineHeight="27px"
                 fontWeight={500}
-                color={"rgba(255, 255, 255, 0.5)"}
+                color={'rgba(255, 255, 255, 0.5)'}
                 zIndex={200000}
                 textAlign="left"
               >
@@ -109,18 +118,18 @@ const Section4 = () => {
                 fontWeight={400}
                 color="#ffffff"
                 zIndex={1000}
-                width={"100%"}
+                width={'100%'}
                 borderRadius="10px"
                 padding="22px, 39px, 22px, 39px"
                 backgroundColor="#3E86F5"
-                fontSize={"14px"}
+                fontSize={'14px'}
                 lineHeight="21px"
-                _active={{ border: "none" }}
-                _focus={{ border: "none" }}
-                border={"none"}
+                _active={{ border: 'none' }}
+                _focus={{ border: 'none' }}
+                border={'none'}
                 sx={{
                   _hover: {
-                    transform: "scale(.97)",
+                    transform: 'scale(.97)',
                   },
                 }}
               >
@@ -128,7 +137,7 @@ const Section4 = () => {
               </Button>
             </VStack>
           </Box>
-          <Image src={snow} height={"500px"} mb={6} objectFit="cover" />
+          <Image src={snow} height={'500px'} mb={6} objectFit="cover" />
         </Flex>
       </VStack>
     </Box>
